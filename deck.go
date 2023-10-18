a094375d077e007c291ac150d75d61870bc562fb
DurDu54 <mucahitdurdu54@gmail.com>
2023-10-19 01:20:28 +0300

    Save the deck as a file

--- deck.go
+++ deck.go
@@ -1,13 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 // Create a new typr of 'deck'
 // wich is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+
+}
+
+func (d deck) saveToFile(fileName string) error {
+	return os.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(fileName string) deck {
+	bs, err := os.ReadFile(fileName)
+	if err != nil {
+		// Option #1 — log the error and return a call to newDeck()
+		// Option #2 — Log the error and entirely guit the program
+
+		fmt.Println("Error", err)
+		os.Exit(1)
+	}
+
 }
--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-	cards.print()
+	cards := newDeck()
+	cards.saveToFile("my_cards")
 }
+
 func newCard() string {
 	return "Five of Diamonds"
 }
